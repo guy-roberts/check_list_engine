3b1aad7606928e77e56d2ce6af3278fe1333e823
Guy Roberts <groberts2001@hotmail.com>
2018-03-13 12:05:48 +0000

    Added missing attrs

--- app/serializers/audit_type_component_serializer.rb
+++ app/serializers/audit_type_component_serializer.rb
@@ -2,5 +2,8 @@ class AuditTypeComponentSerializer
   include FastJsonapi::ObjectSerializer
 
   set_type :audit_type_component
-  attributes :position, :title, :help_text
+  attributes :position, :title, :help_text, :choices, :has_image, :is_mandatory
+
+  has_one :available_component
+  
 end
