be4933dac84a1558a9866d848dd12472a24b8cb6
Guy Roberts <groberts2001@hotmail.com>
2018-03-26 19:40:14 +0100

    Expose the name of the component type

--- app/resources/check_list_engine/api/audit_type_component_resource.rb
+++ app/resources/check_list_engine/api/audit_type_component_resource.rb
@@ -3,7 +3,7 @@ module CheckListEngine
     class AuditTypeComponentResource < JSONAPI::Resource
       model_name 'CheckListEngine::AuditTypeComponent'
       attributes :title, :position, :help_text, :has_image, :choices, :is_mandatory
-      #attribute :name_of_component_type
+      attribute :name_of_component_type
 
       has_one :audit_type
       has_one :available_component
